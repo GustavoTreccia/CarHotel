f0e7eef01b5fd9aa34263d96465591f825fa57e0
GustavoTreccia <g.treccia@outlook.com>
2021-12-01 22:39:50 -0300

    Start project

---
+++ CarHotel_Backend/src/main/java/com/one/demo/CarHotelApplication.java
@@ -0,0 +1,13 @@
+package com.one.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarHotelApplication.class, args);
+	}
+
+}
---
+++ CarHotel_Backend/src/main/java/com/one/demo/entities/CarReg.java
@@ -0,0 +1,187 @@
+package com.one.demo.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_carsRegistry")
+public class CarReg implements Serializable{
+
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String marca;
+	private String modelo;
+	private String tipo;
+	private String placa;
+	private String apelidoProprietario;
+	private Date dataEntrada;
+	private Date dataSaida;
+	
+	public CarReg() {}
+
+	public CarReg(Long id, String marca, String modelo, String tipo, String placa, String apelidoProprietario,
+			Date dataEntrada, Date dataSaida) {
+		super();
+		this.id = id;
+		this.marca = marca;
+		this.modelo = modelo;
+		this.tipo = tipo;
+		this.placa = placa;
+		this.apelidoProprietario = apelidoProprietario;
+		this.dataEntrada = dataEntrada;
+		this.dataSaida = dataSaida;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public String getApelidoProprietario() {
+		return apelidoProprietario;
+	}
+
+	public void setApelidoProprietario(String apelidoProprietario) {
+		this.apelidoProprietario = apelidoProprietario;
+	}
+
+	public Date getDataEntrada() {
+		return dataEntrada;
+	}
+
+	public void setDataEntrada(Date dataEntrada) {
+		this.dataEntrada = dataEntrada;
+	}
+
+	public Date getDataSaida() {
+		return dataSaida;
+	}
+
+	public void setDataSaida(Date dataSaida) {
+		this.dataSaida = dataSaida;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((apelidoProprietario == null) ? 0 : apelidoProprietario.hashCode());
+		result = prime * result + ((dataEntrada == null) ? 0 : dataEntrada.hashCode());
+		result = prime * result + ((dataSaida == null) ? 0 : dataSaida.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((marca == null) ? 0 : marca.hashCode());
+		result = prime * result + ((modelo == null) ? 0 : modelo.hashCode());
+		result = prime * result + ((placa == null) ? 0 : placa.hashCode());
+		result = prime * result + ((tipo == null) ? 0 : tipo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CarReg other = (CarReg) obj;
+		if (apelidoProprietario == null) {
+			if (other.apelidoProprietario != null)
+				return false;
+		} else if (!apelidoProprietario.equals(other.apelidoProprietario))
+			return false;
+		if (dataEntrada == null) {
+			if (other.dataEntrada != null)
+				return false;
+		} else if (!dataEntrada.equals(other.dataEntrada))
+			return false;
+		if (dataSaida == null) {
+			if (other.dataSaida != null)
+				return false;
+		} else if (!dataSaida.equals(other.dataSaida))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (marca == null) {
+			if (other.marca != null)
+				return false;
+		} else if (!marca.equals(other.marca))
+			return false;
+		if (modelo == null) {
+			if (other.modelo != null)
+				return false;
+		} else if (!modelo.equals(other.modelo))
+			return false;
+		if (placa == null) {
+			if (other.placa != null)
+				return false;
+		} else if (!placa.equals(other.placa))
+			return false;
+		if (tipo == null) {
+			if (other.tipo != null)
+				return false;
+		} else if (!tipo.equals(other.tipo))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "CarReg [id=" + id + ", marca=" + marca + ", modelo=" + modelo + ", tipo=" + tipo + ", placa=" + placa
+				+ ", apelidoProprietario=" + apelidoProprietario + ", dataEntrada=" + dataEntrada + ", dataSaida="
+				+ dataSaida + "]";
+	}
+	
+	
+	
+	
+
+}
